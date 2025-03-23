81160f499e40f5d14aa581a1e007fa0f12c670fd
Rose Momanyi <rosebmomanyi@gmail.com>
2025-03-23 17:14:30 +0300

    Created a function that loads all movies

--- index.js
+++ index.js
@@ -21,5 +21,28 @@ document.addEventListener ('DOMContentLoaded', ()=> {
         buyTicketsBtn.onclick=()=>buyTicket(movie, availableTickets)
 
 
+
     }
+
+    function loadAllMovies () {
+        fetch("http://localhost:3000/films")
+        .then ((response) => response.json)
+        .then (movies => {
+            movieList.innerHTML=''
+            movies.forEach (movie =>{
+                let li = document.createElement('li')
+                li.textContent = movie.title
+                li.classList.add('film', 'item')
+                li.onclick = ()=> fetch (`http://localhost:3000/films/${movie.id}`)
+                                    .then (response => response.json())
+                                    .then (displayMovieDetails)
+
+                movieList.appendChild(li)
+            })
+    );
+        })
+
+    }
+
+
 })
