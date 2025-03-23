cdecfa2a0992b60317935bc6eacdca77a72d4757
Rose Momanyi <rosebmomanyi@gmail.com>
2025-03-23 16:57:21 +0300

    Added a function to display movie details

--- index.js
+++ index.js
@@ -9,4 +9,17 @@ document.addEventListener ('DOMContentLoaded', ()=> {
         .then (movie => displayMovieDetails(movie))
 
     }
+
+    function displayMovieDetails (movie) {
+        document.getElementById('poster').src=movie.poster
+        document.getElementById('title').textContent=movie.title
+        document.getElementById('runtime').textContent=movie.runtime
+        document.getElementById('showtime').textContent=movie.showtime
+
+        let availableTickets = movie.capacity - movie.tickets_sold
+        document.getElementById('available-tickets').textContent = availableTickets
+        buyTicketsBtn.onclick=()=>buyTicket(movie, availableTickets)
+
+
+    }
 })
