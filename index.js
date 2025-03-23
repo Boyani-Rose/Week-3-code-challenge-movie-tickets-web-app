0a65d8662f97c6cec0b52cbe0f75f00ce91e7996
Rose Momanyi <rosebmomanyi@gmail.com>
2025-03-23 17:50:21 +0300

    Made changes in index.js to render the list of films

--- index.js
+++ index.js
@@ -13,11 +13,23 @@ document.addEventListener ('DOMContentLoaded', ()=> {
     function displayMovieDetails (movie) {
         document.getElementById('poster').src=movie.poster
         document.getElementById('title').textContent=movie.title
-        document.getElementById('runtime').textContent=movie.runtime
+        document.getElementById('runtime').textContent=movie.runtime = `${movie.runtime} minutes`
         document.getElementById('showtime').textContent=movie.showtime
 
         let availableTickets = movie.capacity - movie.tickets_sold
         document.getElementById('available-tickets').textContent = availableTickets
+        buyTicketsBtn.textContent = "Buy Ticket";
+        buyTicketsBtn.disabled = false;
+
+        if (availableTickets === 0) {
+            buyTicketsBtn.textContent = "Sold Out";
+            buyTicketsBtn.disabled = true;}
+
+
+
+
+
+
         buyTicketsBtn.onclick=()=>buyTicket(movie, availableTickets)
 
 
@@ -26,7 +38,7 @@ document.addEventListener ('DOMContentLoaded', ()=> {
 
     function loadAllMovies () {
         fetch("http://localhost:3000/films")
-        .then ((response) => response.json)
+        .then ((response) => response.json ())
         .then (movies => {
             movieList.innerHTML=''
             movies.forEach (movie =>{
