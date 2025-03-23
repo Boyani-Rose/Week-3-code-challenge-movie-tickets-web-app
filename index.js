4f72630b54b5ee701f4e329c8a14c2b80b89e81b
Rose Momanyi <rosebmomanyi@gmail.com>
2025-03-23 16:37:38 +0300

    Created a function that had fetch, to load the first movie

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ document.addEventListener ('DOMContentLoaded', ()=> {
     const movieDetails = document.getElementById('movie-details')
     const movieList = document.getElementById('films')
     const buyTicketsBtn = document.getElementById('buy-ticket')
+
+    function loadFirstMovie () {
+        fetch("http://localhost:3000/films/1")
+        .then((response) => response.json())
+        .then (movie => displayMovieDetails(movie))
+
+    }
 })
