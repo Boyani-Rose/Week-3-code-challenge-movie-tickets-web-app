178104d0cd6f44439df7e5c0e733d85c56b0a04d
Rose Momanyi <rosebmomanyi@gmail.com>
2025-03-23 16:21:55 +0300

    Created an event listener on DOM content loaded and created a callback function with variables inside

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener ('DOMContentLoaded', ()=> {
+    const movieDetails = document.getElementById('movie-details')
+    const movieList = document.getElementById('films')
+    const buyTicketsBtn = document.getElementById('buy-ticket')
+})
